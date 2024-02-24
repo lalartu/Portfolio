c81a071a5fa721fc2faad4b014fc97c543239321
Anna <dev-charodeyka@proton.me>
2024-02-24 22:05:55 +0100

    feat: add docs folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
-
+/* import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; */
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  kit: {
+    adapter: adapter(
+      {pages: 'docs',
+      assets: 'docs',
+      fallback: undefined,
+      precompress: false,
+      strict:true}
+,    )
+  },
+  /* preprocess: vitePreprocess() */
 };
-
 export default config;
