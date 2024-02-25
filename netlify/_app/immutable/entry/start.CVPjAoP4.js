83b1012501dca31bccb8306016229c3ac23487d4
Anna <dev-charodeyka@proton.me>
2024-02-25 02:02:41 +0100

    img to webp

--- netlify/_app/immutable/entry/start.CVPjAoP4.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Dv-C0RZo.js";export{t as start};
--- src/lib/commands/whoareyou.svelte
+++ src/lib/commands/whoareyou.svelte
@@ -5,14 +5,14 @@
   <div class="flex justify-center">
     <div class="flex justify-between pl-4 gap-10">
       <div class="flex justify-start w-1/3 items-center gap-6">
-        <img src="dev.png" alt="me" class="object-contain h-24" />
+        <img src="dev.webp" alt="me" class="object-contain h-24" />
         <div class="flex flex-col items-start">
           <h1 class="text-topbarViola font-bold font-base">Dev:</h1>
           <h1>Me</h1>
         </div>
       </div>
       <div class="w-2/3 flex justify-end items-center gap-6">
-        <img src="charodeyka.png" alt="horse" class="object-contain h-24" />
+        <img src="charodeyka.webp" alt="horse" class="object-contain h-24" />
         <div class="flex flex-col items-start w-56">
           <h1 class="text-topbarViola font-bold font-base">Charodeyka:</h1>
           <h1>My horse</h1>
