b3da121671dacee715922a068ad2927f5fb73f12
offdeku <nneverquest@gmail.com>
2024-02-28 22:04:11 +0100

    Fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 /* import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; */
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,6 @@ const config = {
       assets: 'vercel',
       fallback: undefined,
       precompress: false,
-      out: 'public',
       strict:true}
 ,    )
   },
