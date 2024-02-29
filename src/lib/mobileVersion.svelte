6d0d8d3d1e777be2f6f83e49703242ea580373de
offdeku <nneverquest@gmail.com>
2024-02-29 08:53:17 +0100

    Mobile Update

--- src/lib/mobileVersion.svelte
+++ src/lib/mobileVersion.svelte
@@ -3,9 +3,7 @@
 </script>
 
 <div class="font-ProFontIIxNerdFontRegular text-white md:hidden h-fit text-sm p-4">
-    <Typewriter text="Here's my project in action: a personal website with a cool terminal look, the final product of 
-    my tutorial on Medium.
-    If you're seeing this, you're likely on a mobile device, which cannot offer the best experience for this design idea. 
+    <Typewriter text="If you're seeing this, you're likely on a mobile device, which cannot offer the best experience for this design idea. 
     For full experience, please open this site from tablet, laptop, or PC. "/>
 </div>
 
